b44d16e7cbdcb48fef1596df69ef833ace12fe62
Leo Takaoka <tlkanb0908@gmail.com>
2020-09-04 23:06:35 +0900

    mainパッケージから処理の記述を削除

--- main.go
+++ main.go
@@ -2,59 +2,11 @@ package main
 
 import (
 	"flag"
-	"fmt"
-	"os"
-	"path/filepath"
 )
 
-var from := flag.String("from", ".jpeg", "Extension before conversion")
-var to := flag.String("to", ".png", "Extension after conversion")
-
-func convert(dst, src string) error {
-	sf, err := os.Open(src)
-	if err != nil {
-		return fmt.Errorf("ファイルを開けませんでした。 %s", src)
-	}
-	defer sf.Close()
-
-	df, err := os.Create(dst)
-	if err != nil {
-		return fmt.Errorf("ファイルを書き出せませんでした %s", dst)
-	}
-	defer df.Close()
-
-	img, _, err := image.Decode(sf)
-	if err != nil {
-		return err
-	}
-
-	switch strings.ToLower(filepath.Ext(dst)) {
-	case ".png":
-		err = png.Encode(df, img)
-	case ".jpen", ".jpg":
-		err = jpeg.Encode(df, img, &jpeg.Options{Quality: jpeg.DefaultQuality})
-	case ".gif":
-		err = gif.Encode(df, img, nil)
-	}
-
-	if err != nil {
-		return fmt.Errorf("画像ファイルを書き出せませんでした。 %s", dst)
-	}
-
-	return nil
-}
-
-func run() error {
-	if len(os.Args) < 3 {
-		return fmt.Errorf("画像ファイルを指定してください。")
-	}
-
-	return convert(os.Args[2], os.Args[1])
-}
+var from = flag.String("from", ".jpeg", "Extension before conversion")
+var to = flag.String("to", ".png", "Extension after conversion")
 
 func main() {
-	if err := run(); err != nil {
-		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
-		os.Exit(1)
-	}
+	flag.Parse()
 }
