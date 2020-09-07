6c543521595c56f2a372d609482092c1efdde8e3
Leo Takaoka <tlkanb0908@gmail.com>
2020-09-07 22:53:00 +0900

    オプションの変数を短くした

--- main.go
+++ main.go
@@ -9,16 +9,16 @@ import (
 	"github.com/Le0tk0k/imgpro/imgconv"
 )
 
-var from = flag.String("from", ".jpeg", "Extension before conversion")
-var to = flag.String("to", ".png", "Extension after conversion")
-var rmsrc = flag.Bool("rmsrc", false, "Remove file before conversion")
+var from = flag.String("f", ".jpeg", "Extension before conversion")
+var to = flag.String("t", ".png", "Extension after conversion")
+var rm = flag.Bool("f", false, "Remove file before conversion")
 
 func main() {
 	flag.Parse()
 	src := flag.Arg(0)
 	dst := strings.Replace(flag.Arg(0), *from, *to, 1)
 
-	err := imgconv.Convert(src, dst, *rmsrc)
+	err := imgconv.Convert(src, dst, *rm)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
 	}
