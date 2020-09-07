3435a63104a0b1ae6e6dd5c029fa139e16105e55
Leo Takaoka <tlkanb0908@gmail.com>
2020-09-07 15:36:12 +0900

    変換後のファイル名を指定できるようにした

--- main.go
+++ main.go
@@ -11,11 +11,18 @@ import (
 
 var from = flag.String("from", ".jpeg", "Extension before conversion")
 var to = flag.String("to", ".png", "Extension after conversion")
+var name = flag.Bool("name", false, "Filename after conversion")
 
 func main() {
 	flag.Parse()
 	src := flag.Arg(0)
-	dst := strings.Replace(flag.Arg(0), *from, *to, 1)
+	var dst string
+	if *name {
+		dst = flag.Arg(1)
+	} else {
+		dst = strings.Replace(flag.Arg(0), *from, *to, 1)
+	}
+
 	err := imgconv.Convert(src, dst)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err.Error())
